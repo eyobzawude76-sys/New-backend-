d52d7f55821727e82ae0d5328a334504e91f2604
EYOB <eyobzawude76@gmail.com>
2026-05-24 15:22:47 +0300

    xc

--- app/database.py
+++ app/database.py
@@ -3,17 +3,18 @@ import os
 from motor.motor_asyncio import AsyncIOMotorClient
 from dotenv import load_dotenv
 
+# Env variables fiduuf (.env fi Render irraa)
+load_dotenv()
 
-
-# Render irraas ta'e .env irraa kallattiin fida
 MONGO_URL = os.getenv("MONGODB_URL")
 DB_NAME = os.getenv("DATABASE_NAME", "university_db")
 
-# Client kaasuuf
-client = AsyncIOMotorClient(MONGO_URL)
+# 🔥 RAKKOO ATLAS IRRA TURTE HIKUOF:
+# 'w=1' itti dabaluun dogoggora WriteConcernError 'majority' sana guutummaatti dhabamsiisa.
+client = AsyncIOMotorClient(MONGO_URL, w=1)
 db = client[DB_NAME]
 
-# Collections kee guutuu
+# Collections kee guutuu bifa kanaan kaayyarra
 users_collection = db["users"]
 students_collection = db["students"]
 courses_collection = db["courses"]
